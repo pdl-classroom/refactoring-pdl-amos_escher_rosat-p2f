dfa1da92199f16e6c1f1b6d0cbe47862b31ed41c
Cédric <cedric.rosat@me.com>
2023-06-18 10:51:51 +0200

    Updating tests for JSON

--- src/test/java/ch/heigvd/pdl/refactoring/OrdersWriterTest.java
+++ src/test/java/ch/heigvd/pdl/refactoring/OrdersWriterTest.java
@@ -18,13 +18,13 @@ class OrdersWriterTest {
 
     @Test
     void NoOrder() {
-        assertEquals("{\"orders\": []}", new OrdersWriter().getContents(new Orders()));
+        assertEquals("{\"orders\": []}", OrdersWriter.getContents(new Orders()));
     }
 
     @Test
     void OneOrder() {
         String order0 = "{\"id\": 0, \"products\": []}";
-        assertEquals("{\"orders\": [" + order0 + "]}", new OrdersWriter().getContents(orders));
+        assertEquals("{\"orders\": [" + order0 + "]}", OrdersWriter.getContents(orders));
     }
 
     @Test
@@ -32,21 +32,21 @@ class OrdersWriterTest {
         orders.addOrder(new Order());
         String order0Json = JsonOrder0WithProduct("");
         String order1Json = "{\"id\": 1, \"products\": []}";
-        assertEquals("{\"orders\": [" + order0Json + ", " + order1Json + "]}", new OrdersWriter().getContents(orders));
+        assertEquals("{\"orders\": [" + order0Json + ", " + order1Json + "]}", OrdersWriter.getContents(orders));
     }
 
     @Test
     void OneOrderWithOneProduct() {
         order0.addProduct(new Product("Shirt", Color.BLUE, Size.M, 2.99, "TWD"));
-        String order0Json = JsonOrder0WithProduct("{\"code\": \"Shirt\", \"color\": \"blue\", \"size\": \"M\", \"price\": 2.99, \"currency\": \"TWD\"}");
-        assertEquals("{\"orders\": [" + order0Json + "]}", new OrdersWriter().getContents(orders));
+        String order0Json = JsonOrder0WithProduct("{\"code\": \"Shirt\", \"color\": \"BLUE\", \"size\": \"M\", \"price\": 2.99, \"currency\": \"TWD\"}");
+        assertEquals("{\"orders\": [" + order0Json + "]}", OrdersWriter.getContents(orders));
     }
 
     @Test
     void OneOrderWithOneProductNoSize() {
         order0.addProduct(new Product("Pot", Color.RED, Size.NO_SIZE, 16.50, "SGD"));
-        String order0Json = JsonOrder0WithProduct("{\"code\": \"Pot\", \"color\": \"red\", \"price\": 16.5, \"currency\": \"SGD\"}");
-        assertEquals("{\"orders\": [" + order0Json + "]}", new OrdersWriter().getContents(orders));
+        String order0Json = JsonOrder0WithProduct("{\"code\": \"Pot\", \"color\": \"RED\", \"price\": 16.5, \"currency\": \"SGD\"}");
+        assertEquals("{\"orders\": [" + order0Json + "]}", OrdersWriter.getContents(orders));
     }
 
     @Test
